022d0075996a3de63c59a8d0966b388ee6f8088d
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-04-09 23:24:00 +0100

    Some cleanups.

--- controllers/documentation_controller.js
+++ controllers/documentation_controller.js
@@ -188,28 +188,21 @@ jQuery.Controller.extend('DocumentationController',
     },
 
 	load : function(){
-		/*this.find("#menu").phui_menuable()
-		 .find("a").click(function(){
-				$(this).closest("li").trigger("select")
-			});*/
-
-			//menu with focus
-			$("#menu").phui_menuable().find("ul").phui_menuable().hide()
-				.bind("show", function(){
-					$(this).show(function(){
-						$(this).trigger("show:after")
-					});
-				})
-				.bind("hide", function(){
-					$(this).hide(function(){
-						$(this).trigger("hide:after")
-					});
+		$("#menu").phui_menuable().find("ul").phui_menuable().hide()
+			.bind("show", function(){
+				$(this).show(function(){
+					$(this).trigger("show:after")
 				});
-        	$("#menu").find("a").click(function(){
-				$(this).closest("li").trigger("activate")
+			})
+			.bind("hide", function(){
+				$(this).hide(function(){
+					$(this).trigger("hide:after")
+				});
 			});
+    	$("#menu").find("a").click(function(){
+			$(this).closest("li").trigger("activate")
+		});
 
-		
 		var self = this;
 		this.find("#documentation").phui_filler({parent: $(window)});
 		this.find("#bottom").phui_filler();
