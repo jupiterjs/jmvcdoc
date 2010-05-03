ed28760fce0d1053431c9c26cbd351ea16c22d3b
Alexandre Gomes <alexandre.rijo@gmail.com>
2010-05-03 18:02:41 +0100

    Fixed merge conflicts.

--- controllers/documentation_controller.js
+++ controllers/documentation_controller.js
@@ -274,7 +274,6 @@ jQuery.Controller.extend('DocumentationController',
             $('#doc').html("//jmvcdoc/views/favorite.ejs",{})
     },
 
-<<<<<<< HEAD
     ready : function(){
         /*$("#menu").phui_menuable().find("ul").phui_menuable().hide()
             .bind("show", function(){
@@ -298,10 +297,6 @@ jQuery.Controller.extend('DocumentationController',
             if(ev.keyCode == 13 || ev.keyCode == 10)
                 $(this).closest("li").trigger("activate")
         });*/        
-=======
-	ready : function(){
-				
->>>>>>> ba3600ff97dc375afddf3134da1a3f208c952cba
 
         var self = this;
         this.find("#documentation").phui_filler({parent: $(window)});
@@ -323,15 +318,7 @@ jQuery.Controller.extend('DocumentationController',
         this.searchReady = true;
         //do what you would normally do
         $("#search").attr('disabled', false)
-<<<<<<< HEAD
-        $("#search").val(this.loadText);
-        
-        var self = this;
-        setTimeout(function(){
-            self.handleHistoryChange(self.loadHistoryData);
-            $("#search").focus();                
-        },1000);        
-=======
+
         $("#search").val(this.loadText).focus();
 		if(this.loadHistoryData){
 			//need a timeout to allow reset of C function
@@ -341,16 +328,12 @@ jQuery.Controller.extend('DocumentationController',
 				self.handleHistoryChange(self.loadHistoryData);
 			},1)
 		}        
->>>>>>> ba3600ff97dc375afddf3134da1a3f208c952cba
+
     },
     
     handleHistoryChange : function(data){
-<<<<<<< HEAD
-        
-        if(data.search){
-=======
+
 		if(data.search){
->>>>>>> ba3600ff97dc375afddf3134da1a3f208c952cba
             $("#search").val(data.search);
             this.searchCurrent();
             if(!data.who) return;
@@ -379,14 +362,7 @@ jQuery.Controller.extend('DocumentationController',
             dataType: "jsonp"
         });
     },
-<<<<<<< HEAD
-    
-    "history.index subscribe" : function(called, data){
 
-        
-        if(!this.searchReady){ //if search is not ready .. wait until it is
-            this.loadHistoryData = data;
-=======
 	/**
 	 * A history event.  Only want to act if search data is available.
 	 */
@@ -394,7 +370,6 @@ jQuery.Controller.extend('DocumentationController',
 		
 		if(!this.searchReady){ //if search is not ready .. wait until it is
 			this.loadHistoryData = data;
->>>>>>> ba3600ff97dc375afddf3134da1a3f208c952cba
             return;
         }
         this.handleHistoryChange(data)
