4ceb51a5101edca0dbf85012b3f47781edfa6258
Brian Moschel <brian.moschel@gmail.com>
2010-10-10 03:24:22 -0500

    fixing the build script

---
+++ scripts/build.js
@@ -0,0 +1,6 @@
+//steal/js cookbook/scripts/compress.js
+
+load("steal/rhino/steal.js");
+steal.plugins('steal/build','steal/build/scripts',function(){
+	steal.build('jmvcdoc/jmvcdoc.html',{to: 'jmvcdoc'});
+});
